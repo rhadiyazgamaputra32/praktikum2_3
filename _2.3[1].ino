2848401e9d965b92ae356bd93a2a08ab3394b824
rhadiyazgamaputra32 <radiasgama@gmail.com>
2016-09-02 11:27:52 +0700

    praktikum2_3
    praktikum2_3

---
+++ _2.3[1].ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode (12, 1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (12,1);
+delay(1000);
+digitalWrite(12,0);
+delay(1000);
+
+}
